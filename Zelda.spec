2e7726c52060f64628072fa4efee64dae8ddc2c4
Merovee <Merovee@live.fr>
2016-02-29 09:31:58 +0100

    Zelda initial commit
    Zelda reoganize subdirectory according to pattern in order to reaarange pod/date to date/pod (specific domain usage)

---
+++ Zelda.spec
@@ -0,0 +1,29 @@
+import gooey
+gooey_root = os.path.dirname(gooey.__file__)
+gooey_languages = Tree(os.path.join(gooey_root, 'languages'), prefix = 'gooey/languages')
+gooey_images = Tree(os.path.join(gooey_root, 'images'), prefix = 'gooey/images')
+a = Analysis(['Zelda.py'],
+             pathex=['E:\\H_Prog\\B_Python\\Zelda'],
+             hiddenimports=[],
+             hookspath=None,
+             runtime_hooks=None,
+             )
+pyz = PYZ(a.pure)
+
+options = [('u', None, 'OPTION'), ('u', None, 'OPTION'), ('u', None, 'OPTION')]
+
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          options,
+          gooey_languages,
+          gooey_images,
+          name='Zelda',
+          debug=False,
+          strip=None,
+          upx=True,
+          console=False,
+          windowed=True,
+          icon=os.path.join(gooey_root, 'images', 'program_icon.ico'))
---
+++ Zelda_v1.1.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+
+"""
+---------------------------------------
+   Princess Zelda, clean this mess !
+---------------------------------------
+
+Usage:
+    1) Edit Zelda in order to configure it
+    2) Update sourcDir and regex
+    3) in shell : python Zelda.py
+
+---------------------------------------
+Zelda is used to sort pod/dates the pod's mess
+"""
+
+from argparse import ArgumentParser
+
+import os, shutil, re
+import glob
+
+def trier(srcDir, regex):
+
+    # convert string to pattern
+    pattern = re.compile(regex, re.VERBOSE)
+    
+    # get all files in correct raw directory
+    fichiers = glob.glob("{}/???/*.*".format(srcDir))
+
+    # main loop
+    for f in fichiers:
+    
+        # get name/pod/dir according to path
+        f_rep, f_name = os.path.split(f)
+        podnumber = f_rep[-3::]
+        
+        # apply regex in order to get elements
+        mo = pattern.search(f_name)
+        
+        # if regex is KO abandon
+        if mo == None:
+            continue
+        else:
+            moteur = mo.group(1)
+            useless = mo.group(2) 
+            banc = mo.group(3)
+            pod = mo.group(4)
+            useless = mo.group(5)
+            run = mo.group(6)
+            date = mo.group(7)
+            heure = mo.group(8)
+            
+            if date == None:
+                continue
+            else :
+                # build path for destination directory
+                dstDir = os.path.join(srcDir, date, 'pod{}'.format(podnumber))
+                # make directory if necessary
+                if not os.path.exists(dstDir):
+                    os.makedirs(dstDir)
+               
+                srcPath = f
+                # srcPath = os.path.join(srcDir, f_name)
+                dstPath = os.path.join(dstDir, f_name)
+                
+                print 'from {}\nto   {}\n'.format(srcPath, dstPath)
+                # move file from src to dst
+                shutil.move(srcPath, dstPath)
+
+@Gooey
+def main():
+    
+    parser = ArgumentParser()
+    parser.add_argument('pattern', nargs='?', default=r"""(.*)_(.*)_(.*)_(.*)_(.*)_(.*)-(.{8})-(.*).datx_(.*)""")
+    parser.add_argument('srcDir', nargs='?', default=os.getcwd())	
+    
+    args = parser.parse_args()
+
+    trier(args.srcDir, args.pattern)
+		
+if __name__ == '__main__':
+	main()
